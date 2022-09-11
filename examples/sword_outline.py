4a75c69cb0ed881f794732dde4e3dc3c386b2b8a
vfrancois <v.francois@mediaire.de>
2022-09-11 23:56:39 +0200

    update example

--- examples/sword_outline.py
+++ examples/sword_outline.py
@@ -1,25 +1,33 @@
 import os.path
 
+from svgwrite.container import Group
+
 from examples.base import SWORD_PNG_PATH
 
 from pixel2svg import Drawing, pixel2svg
-
 
 if __name__ == '__main__':
     overlay_img = pixel2svg(SWORD_PNG_PATH)
     final_img = Drawing(overlay_img.width, overlay_img.height)
 
     # add some custom style to the output SVG shapes
-    customized_shapes = []
+    group_1 = Group()
+    group_2 = Group()
     for element in overlay_img.elements:
         # by default, pixel2svg groups shapes of same color inside <g> elements
         if element.elementname == 'g':
             for shape in element.elements:
                 shape['fill-opacity'] = 0
                 shape['fill'] = 'none'
-                shape['stroke'] = '#666666'
-                shape['stroke-width'] = 0.02
-                final_img.add(shape)
+                shape['stroke'] = '#ff00ff'
+                shape['stroke-width'] = 0.3
+                group_1.add(shape)
+                shape_2 = shape.copy()
+                shape_2['stroke'] = 'white'
+                shape_2['stroke-width'] = 0.1
+                group_2.add(shape_2)
 
+    final_img.add(group_1)
+    final_img.add(group_2)
     final_img.save_to_path(os.path.join(os.path.dirname(SWORD_PNG_PATH),
                                         'sword_outline.svg'))
